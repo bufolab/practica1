38b052adaf67c07632c0a1f46c34218142c0c478
ernesto <egonzal@gmail.com>
2019-11-26 06:57:25 +0100

    App ejemplo

---
+++ app/src/main/java/com/test/miprimeraapp/FirstActivity.kt
@@ -0,0 +1,59 @@
+package com.test.miprimeraapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class FirstActivity : AppCompatActivity() {
+
+    val TAG =  FirstActivity::class.java.canonicalName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.e(TAG,"Hola estoy en onCreate")
+
+        val boton =this.findViewById<Button>(R.id.botonEntrar)
+        val titulo = this.findViewById<TextView>(R.id.titulo)
+        val usuario = this.findViewById<EditText>(R.id.editUsuario)
+        boton.setOnClickListener {
+            val nombreUsuario = usuario.text
+            if(nombreUsuario.isBlank()) {
+                titulo.text = getString(R.string.ops_esta_vacio_escribe_algo_porfavor)
+            }else {
+                titulo.text = getString(R.string.hello_name,usuario.text)
+            }
+        }
+    }
+
+    
+    override fun onStart() {
+        super.onStart()
+        Log.e(TAG,"Hola estoy en onStart")
+    }
+
+
+    //usuari pot veure l activity!
+    override fun onResume() {
+        super.onResume()
+        Log.e(TAG,"Hola estoy en onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e(TAG,"Hola estoy en onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.e(TAG,"Hola estoy en onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e(TAG,"Hola estoy en onDestroy")
+    }
+}
