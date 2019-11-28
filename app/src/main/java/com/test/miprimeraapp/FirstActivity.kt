15eb4e46eb46d4a281f8a0d2cc960b4d73d1a9b9
ernesto <egonzal@gmail.com>
2019-11-28 21:31:57 +0100

    Ejemplo guardar valores en SharedPreferences

--- app/src/main/java/com/test/miprimeraapp/FirstActivity.kt
+++ app/src/main/java/com/test/miprimeraapp/FirstActivity.kt
@@ -1,59 +1,75 @@
 package com.test.miprimeraapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class FirstActivity : AppCompatActivity() {
 
-    val TAG =  FirstActivity::class.java.canonicalName
+    val TAG = FirstActivity::class.java.canonicalName
+
+    private val PREFERENCE1 = "PREFERENCIA_1"
+    private val PREFERENCE2 = "PREFERENCIA_2"
+    private val PREFERENCE3 = "PREFERENCIA_3"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.e(TAG,"Hola estoy en onCreate")
+        Log.e(TAG, "Hola estoy en onCreate")
 
-        val boton =this.findViewById<Button>(R.id.botonEntrar)
-        val titulo = this.findViewById<TextView>(R.id.titulo)
-        val usuario = this.findViewById<EditText>(R.id.editUsuario)
-        boton.setOnClickListener {
-            val nombreUsuario = usuario.text
-            if(nombreUsuario.isBlank()) {
-                titulo.text = getString(R.string.ops_esta_vacio_escribe_algo_porfavor)
-            }else {
-                titulo.text = getString(R.string.hello_name,usuario.text)
-            }
+        val sharedPreferences = application.getSharedPreferences("GENERAL", Context.MODE_PRIVATE)
+
+        if (sharedPreferences.contains(PREFERENCE1)) preferencia1.setText(
+            sharedPreferences.getString(
+                PREFERENCE1,""
+            )
+        )
+
+        if (sharedPreferences.contains(PREFERENCE2)) preferencia2.isChecked =
+            sharedPreferences.getBoolean(PREFERENCE2, false)
+
+        if (sharedPreferences.contains(PREFERENCE3)) preferencia3.isChecked =
+            sharedPreferences.getBoolean(PREFERENCE3, false)
+
+        guardarButton.setOnClickListener {
+            sharedPreferences.edit().putString(PREFERENCE1, preferencia1.text.toString())
+                .apply()
+            sharedPreferences.edit().putBoolean(PREFERENCE2, preferencia2.isChecked).apply()
+            sharedPreferences.edit().putBoolean(PREFERENCE3, preferencia3.isChecked).apply()
+        }
+
+        eliminarButton.setOnClickListener {
+            sharedPreferences.edit().clear().apply()
         }
     }
 
-    
+
     override fun onStart() {
         super.onStart()
-        Log.e(TAG,"Hola estoy en onStart")
+        Log.e(TAG, "Hola estoy en onStart")
     }
 
 
     //usuari pot veure l activity!
     override fun onResume() {
         super.onResume()
-        Log.e(TAG,"Hola estoy en onResume")
+        Log.e(TAG, "Hola estoy en onResume")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.e(TAG,"Hola estoy en onPause")
+        Log.e(TAG, "Hola estoy en onPause")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.e(TAG,"Hola estoy en onStop")
+        Log.e(TAG, "Hola estoy en onStop")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.e(TAG,"Hola estoy en onDestroy")
+        Log.e(TAG, "Hola estoy en onDestroy")
     }
 }
